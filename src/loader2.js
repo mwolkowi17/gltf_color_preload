302d3fff36287d2d1996b05bc4cf5447f13802d7
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-10-06 16:02:05 +0200

    new model of reloading image

--- src/loader2.js
+++ src/loader2.js
@@ -2,78 +2,81 @@ import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoade
 import * as THREE from 'three';
 import { scene } from './index.js';
 
-export let mixer2= new THREE.AnimationMixer();
+export let mixer2 = new THREE.AnimationMixer();
 export let action2;
 export let model_kurek;
-model_kurek='./zawor_kulowy_three_kula3_kurek.glb';
+model_kurek = './zawor_kulowy_three_kula3_kurek.glb';
 let root;
 let root2;
 const gltfLoader2 = new GLTFLoader()
-    //gltfLoader2.load('./zawor_kulowy_three_move3.glb', (gltf) => {
-    gltfLoader2.load(model_kurek, (gltf) => {
-      root = gltf.scene;
-      const anim = gltf.animations;
-      scene.add(root); //co zrobic z tym?
-      mixer2 = new THREE.AnimationMixer(root);
-      
-      action2 = mixer2.clipAction( anim[ 0 ])
-     
-      //action.play();
-
-     
-    })
-
-    const gltfLoader3 = new GLTFLoader()
-
-const colorbutton = document.getElementById("color_button");
-
-gltfLoader3.load(model_kurek, (gltf) => {
-  root2 = gltf.scene;
+//gltfLoader2.load('./zawor_kulowy_three_move3.glb', (gltf) => {
+gltfLoader2.load(model_kurek, (gltf) => {
+  root = gltf.scene;
   const anim = gltf.animations;
-  scene.add(root2); //co zrobic z tym?
+  scene.add(root); //co zrobic z tym?
   mixer2 = new THREE.AnimationMixer(root);
-  
-  action2 = mixer2.clipAction( anim[ 0 ])
- 
+
+  action2 = mixer2.clipAction(anim[0])
+
   //action.play();
 
- 
+
 })
 
-colorbutton.addEventListener("click", function(){
-  model_kurek="./zawor_kulowy_three_kula3_kurek_blue.glb"
+
+const model_kurek2 = "./zawor_kulowy_three_kula3_kurek_blue.glb";
+const gltfLoader3 = new GLTFLoader()
+
+gltfLoader3.load(model_kurek2, (gltf) => {
+  root2 = gltf.scene;
+  const anim = gltf.animations;
+  //scene.add(root2); //co zrobic z tym?
+  mixer2 = new THREE.AnimationMixer(root);
+
+  action2 = mixer2.clipAction(anim[0])
+
+  //action.play();
+
+
+})
+
+const colorbutton = document.getElementById("color_button");
+colorbutton.addEventListener("click", function () {
+  model_kurek = "./zawor_kulowy_three_kula3_kurek_blue.glb"
   scene.remove(root);
-  gltfLoader2.load(model_kurek, (gltf) => {
+  scene.add(root2);
+  /*gltfLoader2.load(model_kurek, (gltf) => {
     root = gltf.scene;
     const anim = gltf.animations;
     //root.scale.x=2;
     scene.add(root); //co zrobic z tym?
     mixer2 = new THREE.AnimationMixer(root);
-    
-    action2 = mixer2.clipAction( anim[ 0 ])
-   
+
+    action2 = mixer2.clipAction(anim[0])
+
     //action.play();
 
-   
-  })
+
+  })*/
 }, false);
 
 const colorbackbutton = document.getElementById("color_back_button");
 
-colorbackbutton.addEventListener("click", function(){
-  model_kurek='./zawor_kulowy_three_kula3_kurek.glb';
-  scene.remove(root);
-  gltfLoader2.load(model_kurek, (gltf) => {
+colorbackbutton.addEventListener("click", function () {
+  model_kurek = './zawor_kulowy_three_kula3_kurek.glb';
+  scene.remove(root2);
+  scene.add(root);
+  /*gltfLoader2.load(model_kurek, (gltf) => {
     root = gltf.scene;
     const anim = gltf.animations;
     //root.scale.x=2;
     scene.add(root); //co zrobic z tym?
     mixer2 = new THREE.AnimationMixer(root);
-    
-    action2 = mixer2.clipAction( anim[ 0 ])
-   
+
+    action2 = mixer2.clipAction(anim[0])
+
     //action.play();
 
-   
-  })
-},false)
+
+  })*/
+}, false)
